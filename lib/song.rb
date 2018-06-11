ec32daa61a1dd77401ed53845aae5225901f3267
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-11 13:16:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,5 @@
 class Song
-  def name(ninety_nine_problems)
-    @name = ninety_nine_problems 
-  end
-  def name
-    @name
-  end
+
 end
 
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+
